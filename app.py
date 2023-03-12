a54c23eaa66805e4209f6619fddfaa7fe4505e3f
sandra meera mathew <126272826+sandrameeramathew@users.noreply.github.com>
2023-03-12 23:31:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ add_bg_from_local('ss.jpg')
  
 df = pd.read_csv('train.csv')
 
-X_train, X_test, y_train, y_test = train_test_split(df[['date','store', 'item']], df['sales'], test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(df[['store', 'item']], df['sales'], test_size=0.2, random_state=42)
 model = DecisionTreeRegressor()
 model.fit(X_train, y_train)
 
@@ -39,7 +39,7 @@ print("r2:", accuracy)
 date = '2023-03-12' # Example date
 store = 1 # Example store number
 item = 1 # Example item number
-prediction = model.predict([[date,store, item]])
+prediction = model.predict([[store, item]])
 print("Prediction:", prediction)
 import streamlit as st
 
