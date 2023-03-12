356a0bade831c54aae4a4f9fc5f97a9ee0dcc804
sandra meera mathew <126272826+sandrameeramathew@users.noreply.github.com>
2023-03-12 22:00:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,15 @@
 import pandas as pd
 import numpy as np
-from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
-
-from sklearn.ensemble import RandomForestRegressor
+import streamlit as st
 from sklearn.model_selection import train_test_split
 df = pd.read_csv('train.csv')
-
 X_train, X_test, y_train, y_test = train_test_split(df[['store', 'item']], df['sales'], test_size=0.2, random_state=42)
 model = RandomForestRegressor()
 model.fit(X_train, y_train)
-
 accuracy = model.score(X_test, y_test)
 print("r2:", accuracy)
-date = '2023-03-12' # Example date
-store = 1 # Example store number
-item = 1 # Example item number
+
 prediction = model.predict([[store, item]])
 print("Prediction:", prediction)
 import streamlit as st
