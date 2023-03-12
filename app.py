32e5ce8ef060c6f6e865e0d1ef70ae079577cb96
sandra meera mathew <126272826+sandrameeramathew@users.noreply.github.com>
2023-03-12 23:41:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 import pandas as pd
 from sklearn.linear_model import LinearRegression
@@ -7,7 +6,7 @@ from sklearn.metrics import mean_squared_error, r2_score
 import matplotlib.pyplot as plt
 import base64
 from sklearn.model_selection import train_test_split
-from datetime import datetime
+
 
 
 def add_bg_from_local(image_file):
@@ -29,30 +28,18 @@ add_bg_from_local('ss.jpg')
  
 df = pd.read_csv('train.csv')
 
-X_train, X_test, y_train, y_test = train_test_split(df[['store', 'item', 'date']], df['sales'], test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(df[['store', 'item']], df['sales'], test_size=0.2, random_state=42)
 model = DecisionTreeRegressor()
-model.fit(X_train[['store', 'item']], y_train)
+model.fit(X_train, y_train)
 
-accuracy = model.score(X_test[['store', 'item']], y_test)
+accuracy = model.score(X_test, y_test)
 print("model used:",model)
 print("r2:", accuracy)
-
-from datetime import datetime
-
-def make_prediction(date, store, item):
-    # Convert date to string
-    date_str = date.strftime('%Y-%m-%d')
-    
-    # Convert date string to datetime object
-    date_obj = datetime.strptime(date_str, '%Y-%m-%d')
-    
-    # Add date, store, and item to the list of features
-    features = [date_obj.year, date_obj.month, date_obj.day, store, item]
-    
-    # Make the prediction
-    prediction = model.predict([features])[0]
-    return prediction
-
+date = '2023-03-12' # Example date
+store = 1 # Example store number
+item = 1 # Example item number
+prediction = model.predict([[store, item]])
+print("Prediction:", prediction)
 import streamlit as st
 
 # Create the input form
@@ -63,5 +50,5 @@ item = st.number_input("Enter the item number", min_value=1, max_value=50)
 
 # Make the prediction
 if st.button("Predict"):
-    prediction = make_prediction(date, store, item)
+    prediction = model.predict([[store, item]])
     st.write("The predicted sales for {} at store {} for item {} is {}".format(date, store, item, prediction))
