0ceeb096b69e8c19edb835adaca23e29a0ab1a81
sandra meera mathew <126272826+sandrameeramathew@users.noreply.github.com>
2023-03-14 12:47:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,9 +26,9 @@ def add_bg_from_local(image_file):
 add_bg_from_local('ss.jpg')  
 
  
-df = pd.read_csv('train.csv')
+df = pd.read_csv('train1.csv')
 
-X_train, X_test, y_train, y_test = train_test_split(df[['store', 'item']], df['sales'], test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(df[['day','store', 'item']], df['sales'], test_size=0.2, random_state=42)
 model = DecisionTreeRegressor()
 model.fit(X_train, y_train)
 
@@ -38,7 +38,7 @@ print("r2:", accuracy)
 date = '2023-03-12' # Example date
 store = 1 # Example store number
 item = 1 # Example item number
-prediction = model.predict([[store, item]])
+prediction = model.predict([[day,store, item]])
 print("Prediction:", prediction)
 import streamlit as st
 
@@ -50,5 +50,5 @@ item = st.number_input("Enter the item number", min_value=1, max_value=50)
 
 # Make the prediction
 if st.button("Predict"):
-    prediction = model.predict([[store, item]])
-    st.write("The predicted sales for {} at store {} for item {} is {}".format(date, store, item, prediction))
+    prediction = model.predict([[day,store, item]])
+    st.write("The predicted sales for {} at store {} for item {} is {}".format(day, store, item, prediction))
