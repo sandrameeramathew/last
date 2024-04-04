4ec498e75893c9ac5be280a7a160dc48ddc974cc
sandra meera mathew <126272826+sandrameeramathew@users.noreply.github.com>
2024-04-04 14:57:31 +0530

    app.py

--- app.py
+++ app.py
@@ -8,7 +8,22 @@ import base64
 from sklearn.model_selection import train_test_split
 
 
- 
+ def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"jpg"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+add_bg_from_local('ss.jpg')  
+
 
  
 df = pd.read_csv('train.csv')
